2eabd05a9a09b063e06c3b496a8d9b736361db3b
Thanasis Tsantaris <126119908+Tha-Tsa@users.noreply.github.com>
2024-06-10 03:02:20 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 #coef:the coefficients of the polynomial starting from z^0
 #width,heigth: image resolution
 #real_min,real_max: Maximum and minimum of real axis
 # imag_min, imag_max: Maximum and minimum in the imaginary axis
 # max_iter:Maximum iteration of the loop that iterates the function
+
+
 def julia_set(coef:list, width:float, height:float, real_min:float, real_max:float, imag_min:float, imag_max:float, max_iter:int)-> list:
     real_vals = np.linspace(real_min, real_max, width)
     imag_vals = np.linspace(imag_min, imag_max, height)
     picture = np.zeros((width, height))
+
+    
     #returns the p(z) where p is the polynomial with the specified coefficients
+
     def polynomial(coefficients:list,z:complex):
         return(sum([coefficients[i]*z**i for i in range(len(coefficients))]))
     bound=5/(abs(coef[len(coef)-1])**(1/(len(coef)-1)))
@@ -26,7 +33,9 @@ def julia_set(coef:list, width:float, height:float, real_min:float, real_max:flo
 
 def plot_julia_set(coef, width, height, real_min, real_max, imag_min, imag_max, max_iter):
     picture = julia_set(coef, width, height, real_min, real_max, imag_min, imag_max, max_iter)
-    #this is an array with a number corresponding to each pixel on the screen. Pixels are determined by width and height and each number between 0 and 1 shows what percentage of max iterations each point used before becoming larger than 2. Points in the filled Julia set always use all iterations. 
+    #this is an array with a number corresponding to each pixel on the screen. 
+    # Pixels are determined by width and height and each number between 0 and 1 shows what percentage of max iterations each point used before becoming large enough. 
+    # Points in the filled Julia set always use all iterations. 
     
     plt.imshow(picture, extent=(real_min, real_max, imag_min, imag_max),cmap="plasma")
     #this plots the 2d array. cmap tells you the color, extend tells you the frame dimensions of the picture
